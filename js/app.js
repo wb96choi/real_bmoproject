ab4b761c8c98633a842e7a37703b4dd7beabc1b0
1_b1n <81698301+wb96choi@users.noreply.github.com>
2022-06-23 15:30:22 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,6 +5,8 @@ $(function () {
       let h = d.getHours(); // 시간 변수
       let m = d.getMinutes(); // 분 변수
       let s = d.getSeconds(); // 초 변수
+      let day = d.getDay();
+      let today = '';  
     
       $(".hour").html(h);
       $(".min").html(m);
@@ -27,12 +29,33 @@ $(function () {
           $(".ampm").html(`AM`);
       };
   
+    if(day === 1){
+      today = ('MON');
+    }
+    if(day == 2){
+        today = ('TUE');
+    }
+    if(day === 3){
+        today = ('WED');
+    }
+    if(day == 4){
+        today = ('THU');
+    }
+    if(day == 5){
+        today = ('FRI');
+    }
+    if(day == 6){
+        today = ('SAT');
+    }
+    if(day == 7){
+        today = ('SUN');
+    }
+    $('.weekday').text(today);
+    });
+
     }, 1000);
 
 
-    $(function() {
-      
-    });
-  });
+    
   
   
